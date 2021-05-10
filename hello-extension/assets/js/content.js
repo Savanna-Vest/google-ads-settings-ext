e6981a64f1f09e003889e22a60844530531abbf1
Savanna <savest@davidson.edu>
2021-05-10 18:41:25 -0400

    Additional update to final project
    setTimeOut bug resolved

--- hello-extension/assets/js/content.js
+++ hello-extension/assets/js/content.js
@@ -33,8 +33,6 @@ if (window.location.href.includes ("adssettings.google")) {
     document.body.insertBefore(overlay, document.body.childNodes[0]);
     overlay.innerHTML = "";
     introMain();
-    basicMain();
-    interestMain();
 } else {
     console.log("Hm, it seems you're not on the Google Ads Settings page yet. Why don't you try searching for it?");
 };
@@ -44,74 +42,59 @@ if (window.location.href.includes ("adssettings.google")) {
 // Set of introductory statements to appear in div
 function introHello() {
     return "Hello! I'm your friendly neighorhood Google Ads Settings bot.";
-    let txt = "Hello! I'm your friendly neighorhood Google Ads Settings bot.";
 };
 function introKidding() {
     return "What do I do? What can't I do??? Haha, just kidding.";
-    let txt = "What do I do? What can't I do??? Haha, just kidding.";
 };
 function introProgram() {
     return "I'm a program coded in JavaScript to evaluate your Google Ads Settings page. There's so much information that Google throws at you (if you keep scrolling, you'll see just how long this list is...) that it can be really hard to keep track. So, I'm here to give you a summary.";
-    let txt = "I'm a program coded in JavaScript to evaluate your Google Ads Settings page. There's so much information that Google throws at you (if you keep scrolling, you'll see just how long this list is...) that it can be really hard to keep track. So, I'm here to give you a summary.";
 };
-
 function introPredict() {
     return "It actually is kinda cool, but mostly I think it's pretty scary... especially when you think about how they've predicted this information from your search history on Google AND their partner websites, like YouTube. Not to mention those pesky little cookies that follow us all over the internet. Think you know everything your computer knows about you?";
-    let txt = "It actually is kinda cool, but mostly I think it's pretty scary... especially when you think about how they've predicted this information from your search history on Google AND their partner websites, like YouTube. Not to mention those pesky little cookies that follow us all over the internet. Think you know everything your computer knows about you?";
 };
-
 function introFindOut() {
     return "Well, why don't we find out? You tell me if it's accurate or not. Are you ready to get started?";
-    let txt = "Well, why don't we find out? You tell me if it's accurate or not. Are you ready to get started?";
 };
 
 let introArr = [introHello, introKidding, introProgram, introPredict, introFindOut];
 
 let count = 0
 
-// add html or text to the page
+// Add HTML or text to the page
 function addNode(str) {
-    // get the target element
+    // Get the target element
     var target = document.getElementById("overlay");
-    // add html to target
+    // Add html to target
     target.innerHTML += `<div class="overlay-text-node">${str}</div>`;
 };
 
-function showNextResult(duration = 3000) {
+// Trigger timer for statements for introduction
+function showNextResult(duration = 2500) {
     setTimeout(function () {
-      // if there are functions left
+      // If there are functions left
       console.log("called the thing");
       if (count < introArr.length) {
-        // add the text
+        // Add the text
         addNode(introArr[count]());
         count++;
-        showNextResult(3000);
+        showNextResult(2500);
         console.log("did it work?");
       } 
-      // add an exit button 
+      // Add a progress button 
       else if (count == introArr.length) {
-        // add exit button and listener
+        // Add progress button and listener
         addNode(`<button class="exitBtn" onclick="">I guess so...!</button>`);
         document.querySelector(".exitBtn").addEventListener("click", function () {
-          exit();
+          clearBox(overlay);
+          basicMain();
         });
-        clearBox(overlay);
-        window.alert("Let's take a look at what Google considers to be your basic identity traits. Google may know more about you than you realize...");
-        };
+    };
     }, duration);
 };
 
-function exit() {
-  // remove the event handler
-  document.querySelector(".exitBtn").removeEventListener("click", exit);
-  // then remove node
-  document.querySelector(".overlay-wrapper").remove();
-  // reset count
-  count = 0;
-};
-
+// Wrapper for introduction sequence
 function introMain() {
-    showNextResult(1000);
+    showNextResult(2500);
 };
 
 // PART 2: Basic User Data
@@ -131,13 +114,11 @@ if (window.location.href.includes ("adssettings.google")) {
     verifyAdsData();
 };
 
-
 // Demographics
-
 
 function age() {
     var dem = document.querySelectorAll(".c7O9k");
-    //age
+    // Age
     for (let i = 0; i < dem.length; i++) {
         if (dem[i].textContent.includes("years old")) {
             return "So, let me get this straight: You're " + dem[i].textContent + "?";
@@ -147,7 +128,7 @@ function age() {
 
 function assignedSex() {  
     var dem = document.querySelectorAll(".c7O9k");
-    //sex
+    // Sex
     for (let i = 0; i < dem.length; i++) {
         if (dem[i].textContent.includes("Female")) {
             return "So, Google thinks you're a woman. Whether they're right or not, do you know why they include this in your ads profile? So that they can predictively show you targeted ads that reinforce gender norms. Gross, isn't it?";
@@ -161,7 +142,7 @@ function assignedSex() {
 
 function parentalStatus() {  
     var dem = document.querySelectorAll(".c7O9k");
-    //parental status
+    // Parental status
     for (let i = 0; i < dem.length; i++) {
         if (dem[i].textContent.includes("Parental Status")) {
             return "Your " + dem[i].textContent + ". Sound correct?";
@@ -171,7 +152,7 @@ function parentalStatus() {
 
 function householdIncome() {  
     var dem = document.querySelectorAll(".c7O9k");
-    //household income
+    // Household income
     for (let i = 0; i < dem.length; i++) {
         if (dem[i].textContent.includes("Household Income")) {
             return "Your " + dem[i].textContent + ". Did you know Google made these estimates about you? It's part of how they determine which price range of ads they'll show you, so that you\'re more likely to buy things... Yeah, technocapitalism is creepy and predatory.";
@@ -181,7 +162,7 @@ function householdIncome() {
 
 function maritalStatus() {
     var dem = document.querySelectorAll(".c7O9k");
-    //marital/relationship status
+    // Marital/relationship status
     for (let i = 0; i < dem.length; i++) {
         if (dem[i].textContent.includes("Single")) {
             return "Google thinks you're single. Have you been getting any ads for dating apps? That might be why.";
@@ -195,7 +176,7 @@ function maritalStatus() {
 
 function homeownershipStatus() {
     var dem = document.querySelectorAll(".c7O9k");
-    // homeownership status
+    // Homeownership status
     for (let i = 0; i < dem.length; i++) {
         if (dem[i].textContent.includes("Renters")) {
             return "Google believes you're a renter--or, in other words, that you don't own a home. This can be an added indicator of your income level and age, and perhaps helps Google advertise certain kinds of insurance to you.";
@@ -207,7 +188,7 @@ function homeownershipStatus() {
 
 function educationStatus() {
     var dem = document.querySelectorAll(".c7O9k");
-    // education status
+    // Education status
     for (let i = 0; i < dem.length; i++) {
         if (dem[i].textContent.includes("Current College Students")) {
             return "You're a college student! Do you get lots of ads for applying to college programs, too?";
@@ -223,7 +204,7 @@ function educationStatus() {
 
 function jobIndustry() {
     var dem = document.querySelectorAll(".c7O9k");
-    // job industry
+    // Job industry
     for (let i = 0; i < dem.length; i++) {
         if (dem[i].textContent.includes("Construction Industry")) {
             return "So you work in the construction industry, or want to? Did Google get it right?";
@@ -249,43 +230,46 @@ let basicArr = [age, assignedSex, parentalStatus, householdIncome, maritalStatus
 
 count2 = 0
 
-function showBasicResult(duration = 3000) {
+// Trigger timer for statements for basic information sequence
+
+function showBasicResult(duration = 2500) {
     setTimeout(function () {
-      // if there are functions left
+      // If there are functions left
       console.log("did the second one run?");
       if (count2 < basicArr.length) {
-        // add the text
+        // Add the text
         addNode(basicArr[count2]());
         count2++;
-        showBasicResult(3000);
+        showBasicResult(2500);
         console.log("did the second work?");
       } 
-      // add an exit button 
+      // Add a progress button 
       else if (count2 == basicArr.length) {
-        // add exit button and listener
+        // Add progress button and listener
         addNode(`<button class="exitBtn">Wow. That's a lot...</button>`);
         document.querySelector(".exitBtn").addEventListener("click", function () {
-          exit();
+            clearBox(overlay);
+            interestMain();
         });
-        clearBox(overlay);
-        window.alert("...But that's not all. Google has also identified hundreds of your interests. Let's take a look.");
         };
     }, duration);
 };
 
+// Wrapper function for basic information sequence
 function basicMain(){
-    showBasicResult(20000);
+    window.alert("Let's take a look at what Google considers to be your basic identity traits. Google may know more about you than you realize...");
+    showBasicResult(2500);
 };
 
 // PART 3: User Interests
 
 function interestMusic() {  
     var dem = document.querySelectorAll(".c7O9k");
-    // music
+    // Music
     for (let i = 0; i < dem.length; i++) {
         if (dem[i].textContent.includes("Music")) {
             return "Wow, you really love " + dem[i].textContent + ", huh?";
-        } else if (i === dem.length) {
+        } else if (i === dem.length && "undefined") {
             return "";
         };
     };
@@ -293,11 +277,11 @@ function interestMusic() {
 
 function interestGames() {  
     var dem = document.querySelectorAll(".c7O9k");
-    // games/gaming
+    // Games/gaming
     for (let i = 0; i < dem.length; i++) {
         if (dem[i].textContent.includes("Games" || "Gaming")) {
             return "Apparently you're really into " + dem[i].textContent + ". ...I certainly never would've guessed.";
-        } else if (i === dem.length) {
+        } else if (i === dem.length && "undefined") {
             return "";
         };
     };
@@ -305,11 +289,11 @@ function interestGames() {
 
 function interestFilm() {  
     var dem = document.querySelectorAll(".c7O9k");
-    // films
+    // Films
     for (let i = 0; i < dem.length; i++) {
         if (dem[i].textContent.includes("Film")) {
             return "Seeing lots of movies and Netflix... Don't worry buddy, we've all been there in the past year.";
-        } else if (i === dem.length) {
+        } else if (i === dem.length && "undefined") {
             return "";
         };
     };
@@ -317,11 +301,11 @@ function interestFilm() {
 
 function interestFood() {  
     var dem = document.querySelectorAll(".c7O9k");
-    // food/cooking
+    // Food/cooking
     for (let i = 0; i < dem.length; i++) {
         if (dem[i].textContent.includes("Food" || "Cuisine")) {
             return "Hungry? Google seems to think you've really been craving " + dem[i].textContent + ".";
-        } else if (i === dem.length) {
+        } else if (i === dem.length && "undefined") {
             return "";
         };
     };
@@ -329,11 +313,11 @@ function interestFood() {
 
 function interestClothing() {  
     var dem = document.querySelectorAll(".c7O9k");
-    // clothing
+    // Clothing
     for (let i = 0; i < dem.length; i++) {
         if (dem[i].textContent.includes("Parental Status")) {
             return "Seeing lots of online clothes shopping... That may explain the fashion brands appearing in your ads settings, too.";
-        } else if (i === dem.length) {
+        } else if (i === dem.length && "undefined") {
             return "";
         };
     };
@@ -341,11 +325,11 @@ function interestClothing() {
 
 function interestFitness() {  
     var dem = document.querySelectorAll(".c7O9k");
-    // health/fitness/outdoors
+    // Health/fitness/outdoors
     for (let i = 0; i < dem.length; i++) {
         if (dem[i].textContent.includes("Health" || "Fitness" || "Sport" || "Garden")) {
             return "You've been on a health kick! How's " + dem[i].textContent + " been treating you?";
-        } else if (i === dem.length) {
+        } else if (i === dem.length && "undefined") {
             return "";
         };
     };
@@ -353,11 +337,11 @@ function interestFitness() {
 
 function interestArts() {  
     var dem = document.querySelectorAll(".c7O9k");
-    // arts
+    // Arts
     for (let i = 0; i < dem.length; i++) {
         if (dem[i].textContent.includes("Art" || "Perform" || "Theater" || "Dance" || "Poetry" || "DIY")) {
             return "A connoisseur of the arts! Google can tell you've really been getting into " + dem[i].textContent + " during the pandemic.";
-        } else if (i === dem.length) {
+        } else if (i === dem.length && "undefined") {
             return "";
         };
     };
@@ -365,11 +349,11 @@ function interestArts() {
 
 function interestCare() {  
     var dem = document.querySelectorAll(".c7O9k");
-    // skincare/haircare/etc.
+    // Skincare/haircare/etc.
     for (let i = 0; i < dem.length; i++) {
         if (dem[i].textContent.includes("Care" || "Hair" || "Skin" || "Nail")) {
             return "You've been practicing your " + dem[i].textContent + " lately. Good for you!";
-        } else if (i === dem.length) {
+        } else if (i === dem.length && "undefined") {
             return "";
         };
     };
@@ -381,7 +365,7 @@ function interestTv() {
     for (let i = 0; i < dem.length; i++) {
         if (dem[i].textContent.includes("Shows")) {
             return "Okay, there's a TON I found here about you watching " + dem[i].textContent + ". Are they really that good?";
-        } else if (i === dem.length) {
+        } else if (i === dem.length && "undefined") {
             return "";
         };
     };
@@ -389,11 +373,11 @@ function interestTv() {
 
 function interestWeb() {  
     var dem = document.querySelectorAll(".c7O9k");
-    // web/digital/internet
+    // Web/digital/internet
     for (let i = 0; i < dem.length; i++) {
         if (dem[i].textContent.includes("Web")) {
             return "And, as predicted, lots of time on the internet... You doing okay? Maybe it's time to take a break from the screens.";
-        } else if (i === dem.length) {
+        } else if (i === dem.length && "undefined") {
             return "";
         };
     };
@@ -403,38 +387,43 @@ let interestArr = [interestMusic, interestGames, interestFilm, interestFood, int
 
 count3 = 0
 
-function showInterestResult(duration = 3000) {
+// Trigger timer for statements for interests sequence
+
+function showInterestResult(duration = 2500) {
     setTimeout(function () {
-      // if there are functions left
+      // If there are functions left
       console.log("did the third one run?");
       if (count3 < interestArr.length) {
-        // add the text
+        // Add the text
         addNode(interestArr[count3]());
         count3++;
-        showInterestResult(3000);
+        showInterestResult(2500);
         console.log("did the third work?");
       } 
-      // add an exit button 
+      // Add a progress button 
       else if (count3 == interestArr.length) {
-        // add exit button and listener
+        // Add progress button and listener
         addNode(`<button class="exitBtn">That's a lot to take in...</button>`);
         document.querySelector(".exitBtn").addEventListener("click", function () {
-          exit();
-        });
-        clearBox(overlay);
-        window.alert("...So, how much did Google get right?");
-        window.alert("There's a lot more that goes into Google's ads settings than meets the eye. This was just a sliver of what they've collected about you.");
-        window.alert("If you didn't know before, you do now. And there's still so much more we don't know yet about how Google uses our information.");
-        window.alert("This isn't to say stop using the internet. (That's kind of impossible.)");
-        window.alert("But hopefully you've caught a glimpse at the more insidious side of Google--the side that seeks to profit off of us... Well, off of you humans. I'm a bot.");
-        window.alert("This project was created by Savanna Vest for DIG 345: Radical Software.");
-        window.alert("Check out the latest development in Google's ad settings/data tracking here: https://www.wired.com/story/google-floc-privacy-ad-tracking-explainer/");
+            clearBox(overlay);
+            window.alert("...So, how much did Google get right?");
+            window.alert("There's a lot more that goes into Google's ads settings than meets the eye. This was just a sliver of what they've collected about you.");
+            window.alert("If you didn't know before, you do now. And there's still so much more we don't know yet about how Google uses our information.");
+            window.alert("This isn't to say stop using the internet. (That's kind of impossible.)");
+            window.alert("But hopefully you've caught a glimpse at the more insidious side of Google--the side that seeks to profit off of us... Well, off of you humans. I'm a bot.");
+            window.alert("This project was created by Savanna Vest for DIG 345: Radical Software.");
+            window.alert("Check out the latest development in Google's ad settings/data tracking here: https://www.wired.com/story/google-floc-privacy-ad-tracking-explainer/");
+            var elem = document.getElementById("overlay");
+            elem.parentNode.removeChild(elem);
+            });
         };
     }, duration);
 };
 
+// Wrapper function for interests sequence
 function interestMain(){
-    showInterestResult(50000);
+    window.alert("...But that's not all. Google has also identified hundreds of your interests. Let's take a look.");
+    showInterestResult(2500);
 };
 
 // END
